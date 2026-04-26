ecc0fd1496904da8926edb651e93758e27e95ea2
Tapash Roy <68456825+tktecno@users.noreply.github.com>
2026-04-26 12:06:27 +0530

    Refactor PORT assignment to handle environment variable

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
 import { app } from "./app.js";
 
-<<<<<<< HEAD
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
-=======
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
->>>>>>> fc0350a14070e56e83fc19a84849016e7c3cea96
